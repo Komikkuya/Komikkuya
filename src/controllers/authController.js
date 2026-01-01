b379b1dadce12acee0df3db2be75e7aa7d432287
ItzApipAjalah <70223010+ItzApipAjalah@users.noreply.github.com>
2026-01-01 20:15:03 +0700

    feat: something

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 const FormData = require('form-data');
 
 const AUTH_API_BASE = 'https://komikkuya-backend.vercel.app';
-const HCAPTCHA_SECRET = 'ES_ed8da6c97c95492a8bf0205c1f3e155a';
+const HCAPTCHA_SECRET = '';
 
 // Helper for hCaptcha verification
 const verifyCaptcha = async (token) => {
